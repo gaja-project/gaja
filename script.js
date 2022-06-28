8bb7c49277e1265fed25ca6e24ce913084bad024
Jose <hose153@gmail.com>
2022-06-28 13:26:11 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+changeSubtitleStyle = (vPos, fSize, fColor) => {
+    // logging that our function works
+    console.log("%cnetflix-subtitles-styler: obsvr working")
+
+    callback = () => {
+        // grabs subtitle data from nextflix
+        const subtitles = document.querySelector(".player-timedtext");
+        
+        // styles the location of subtitles
+        if (subtitles) {
+            subtitles.style.bottom = vPos + "px";
+        }
+        console.log(subtitles.data);
+
+        const observer = new MutationObserver(callback);
+        observer.observer(document.body, {
+            subtree: true,
+            attributes: false,
+            childList: true
+        })
+    }
+
+
+}
