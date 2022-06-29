dcabe00e0388efca414248a45e091eceb5cf93d3
sophie <sophie1620@gmail.com>
2022-06-29 18:47:51 -0400

    got access to the subtitles that are on the screen

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
+    changeSubtitleStyle(message.vPos, message.fSize, message.fColor);
+}) 
+
 changeSubtitleStyle = (vPos, fSize, fColor) => {
     // logging that our function works
     console.log("%cnetflix-subtitles-styler: obsvr working")
+
+    // getting access to the subtitles
+    const textContainer = document.querySelector('.player-timedtext-text-container');
+    const textContainerSpan1 = textContainer.firstChild;
+    const textContainerSpan2 = textContainerSpan1.firstChild;
+    const subs = textContainerSpan2.innerText;
+    
+    console.log(subs);
+
+    // to do: need to rewrite the popup.html and popup.js so that when the submit button is clicked, then the extension remains on for the entire duration ~ change the eventListener so that it continuously monitors the subs
+    // Create a function that listens to when the there is a mouseover event in the textContainerSpan2
+        // consult Yvonne to ask if we should make it a toggle button?
+        // 
 
     callback = () => {
         // grabs subtitle data from nextflix
