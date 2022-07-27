449d673c624043c83b1bc3477a5ba9abb198edeb
Jose <hose153@gmail.com>
2022-07-26 17:49:01 -0700

    Updated comments

--- content.js
+++ content.js
@@ -2,10 +2,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     // console.log('the on/off button has been interacted with!', request.message);
     
+    // Window.on_off will be true or false based on the update_on_off value from user
     // if (request.message === "update_on_off" || request.message === "update_font_size") {
     if (request.message === "update_on_off") {
 
-        // Window.on_off will be true or false based on the update_on_off value from user
+        
         // Calls functions to create a dual subtitle container
         window.on_off = request.value;
         if(window.on_off){
